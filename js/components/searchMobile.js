9817b8b44e0e72f377c087e6e60f05291cb41853
Andrey <kolsergienko@gmail.com>
2022-07-13 01:13:50 +0300

    added logic for the mobile form

--- js/components/searchMobile.js
+++ js/components/searchMobile.js
@@ -1,3 +1,9 @@
 const formContainer = document.querySelector('.header__mobile-search');
+const searchButton = document.querySelector('.header__mobile-search-btn');
+const headerSlider = document.querySelector('.header__slider');
 
-console.log(formContainer);
+searchButton.addEventListener('click', mobileFormHandler)
+
+function mobileFormHandler() {
+   formContainer.classList.toggle('shown');
+}
