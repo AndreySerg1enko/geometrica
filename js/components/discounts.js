5c26be5faf82b88372640df6fe7e41cae05891a2
andreuSergienko <kolsergienko@gmail.com>
2022-07-19 21:32:32 +0300

    added slider

---
+++ js/components/discounts.js
@@ -0,0 +1,17 @@
+new Swiper('.discounts-swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
